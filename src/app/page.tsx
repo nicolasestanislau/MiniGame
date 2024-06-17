09c3542d98b54ce7306c275eb3cea232fc6c4e53
Nicolas Estanislau <nicolas_estanislau@hotmail.com>
2024-06-17 07:48:06 -0300

    update new files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from "next/image";
+// pages/index.js
+"use client"
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
+import CrownSvg from '../components/CrownSvg';
+import AwardSvg from '../components/AwardSvg';
+import Modal from '../components/Modal';
 
-export default function Home() {
+
+const generateSequence = (length: number) => {
+  const keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  let sequence = '';
+  for (let i = 0; i < length; i++) {
+    sequence += keys.charAt(Math.floor(Math.random() * keys.length));
+  }
+  return sequence;
+};
+
+interface Score {
+  name: string;
+  score: number;
+}
+
+const Home: React.FC = () => {
+  const [sequence, setSequence] = useState('');
+  const [currentInput, setCurrentInput] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageButton, setMessageButton] = useState('Iniciar');
+  const [timeLeft, setTimeLeft] = useState(30); // Tempo limite de 30 segundos
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [score, setScore] = useState<number>(0);
+  const [ranking, setRanking] = useState<Score[]>([]);
+
+  useEffect(() => {
+    setSequence(generateSequence(6));
+    const savedRanking = localStorage.getItem('ranking');
+
+    if (savedRanking) {
+      setRanking(JSON.parse(savedRanking));
+
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isPlaying && timeLeft > 0) {
+      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timer);
+    } else if (timeLeft === 0) {
+      setIsPlaying(false);
+      setMessage('Tempo esgotado!');
+      saveScore()
+    }
+  }, [timeLeft, isPlaying]);
+
+  const startGame = () => {
+    const newSequence = generateSequence(6);
+    setSequence(newSequence);
+    setCurrentInput('');
+    setMessage('');
+    setTimeLeft(30);
+    setIsPlaying(true);
+    setScore(score);
+    resetProgressBarAnimation();
+  };
+
+  const handleKeyPress = (e: { key: string; }) => {
+    if (!isPlaying) return;
+
+    const key = e.key.toUpperCase();
+    if (sequence[currentInput.length] === key) {
+      setCurrentInput(currentInput + key);
+      if (currentInput.length + 1 === sequence.length) {
+        setMessage('Sucesso');
+        setIsPlaying(false);
+        setMessageButton('Jogar novamente')
+        setScore(score + 1); // Incrementa a pontuação
+        //add sound win
+      }
+    } else {
+      setMessage('Você falhou!');
+      setIsPlaying(false);
+      setMessageButton('Tentar Novamente')
+      setScore(0);
+      saveScore()
+      //add sound lost
+    }
+  };
+
+  const saveScore = () => {
+    const playerName = prompt('Digite seu nome:') || 'Anônimo';
+    const newRanking = [...ranking, { name: playerName, score }];
+    newRanking.sort((a, b) => b.score - a.score);
+    if (newRanking.length > 10) newRanking.pop(); // Mantém apenas os top 10
+    setRanking(newRanking);
+    localStorage.setItem('ranking', JSON.stringify(newRanking));
+  };
+
+  const resetGame = () => {
+    startGame();
+    setScore(0);
+    setSequence(generateSequence(6));
+    setTimeLeft(30);
+    setMessage('');
+    resetProgressBarAnimation();
+  };
+
+  const resetProgressBarAnimation = () => {
+    const progressBar = document.querySelector(`.${styles.progressbar}`) as HTMLElement;
+
+    if (progressBar) {
+      progressBar.classList.remove(styles.progressbar);
+      // Force reflow
+      void progressBar.offsetWidth;
+      progressBar.classList.add(styles.progressbar);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [currentInput, isPlaying]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <div className={styles.svgcontainer}>
+          <CrownSvg />
+        </div>
+        <h1 className={styles.title}>MINI-GAME</h1>
+        {!isPlaying && <div><button className={styles.button} onClick={startGame}>{messageButton}</button></div>}
+        {isPlaying ? <p>Pressione as teclas na ordem correta</p> : null}
+        <div className={styles.sequence}>
+          {isPlaying ? sequence.split('').map((char, index) => (
+            <div
+              key={index}
+              className={`${styles.letterbox} ${currentInput[index] === char ? styles.correct : ''}`}
+            >
+              {char}
+            </div>
+          )) : null}
+
+
+
+        </div>
+        <div>
+          {isPlaying && <button className={styles.buttonreset} onClick={resetGame}>Reiniciar</button>}
+          {isPlaying && <div className={styles.progress}>
+            <div className={styles.progressbar}></div> </div>}
+        </div>
+
+      </div>
+
+      <div className={styles.modal}>
+        {message && <p className={styles.message}>{message}</p>}
+
+        <div className={styles.svgcontainerRanking}>
+          <AwardSvg />
+          <>
+            <p className={styles.description}>Pontuação: {score}</p>
+            <div className={styles.ranking}>
+              <h2>Ranking</h2>
+              <ol>
+                {ranking.map((entry, index) => (
+                  <li key={index}>{entry.name}: {entry.score}</li>
+                ))}
+              </ol>
+            </div>
+          </>
         </div>
       </div>
+    </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
+
+export default Home;
